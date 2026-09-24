80c1a7a8a716e82242516832ea4764e4e82479ac
Casper <yxu114@student.mtsac.edu>
2026-09-24 02:19:50 -0700

    Add initial project structure and implement controller functionality

---
+++ 4_PID/include/controller.h
@@ -0,0 +1,11 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+void controller_screen();
+
+void controller_L1_Pressed();
+void controller_L2_Pressed();
+void controller_R1_Pressed();
+void controller_R2_Pressed();
+
+#endif
