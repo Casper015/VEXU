02af171e95e4ed5780c55a50e52c3497886ca5f5
Casper <yxu114@student.mtsac.edu>
2026-09-25 00:55:48 -0700

    Implement PID controller structure and functions

--- 4_PID/include/vex.h
+++ 4_PID/include/vex.h
@@ -6,8 +6,9 @@
 #include "v5.h"
 #include "v5_vcs.h"
 
-#include "robot-config.h"
 #include "controller.h"
+#include "robot-config.h"
+#include "pid.h"
 
 #define waitUntil(condition)                                                   \
   do {                                                                         \
